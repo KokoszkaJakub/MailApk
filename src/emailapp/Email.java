5df80a22da8f6687dc409da1e98ac062219748b3
KokoszkaJakub <kokoszka.j@gmail.com>
2019-08-28 23:12:35 +0200

    Initial commit

---
+++ src/emailapp/Email.java
@@ -0,0 +1,16 @@
+package emailapp;
+
+public class Email {
+    private String firsName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+    
+
+
+    //sale
+    //development
+    //accounting
+}
