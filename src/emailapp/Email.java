e4ef579cd3dd3f25c20de0ee3e4c38a12a8a987e
KokoszkaJakub <kokoszka.j@gmail.com>
2019-09-02 23:04:20 +0200

    conn test to DB

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,6 +1,10 @@
 package emailapp;
 
 import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Email {
@@ -12,7 +16,7 @@ public class Email {
     private int mailboxCapacity = 750;
     private int defaultPasswordLength = 10;
     private String alternateEmail;
-    private final String companySuffix = "comapny.com";
+    private final String companySuffix = "company.com";
 
     public Email(String firsName, String lastName) {
         this.firsName = firsName;
@@ -82,5 +86,41 @@ public class Email {
                 "\nMAILBOX CAPACITY: " + mailboxCapacity + "mb" +
                 "\nYOUR PASSWORD: " + password;
     }
-    
+    public static Connection getConnection() throws Exception {
+        try {
+            String driver = "com.mysql.cj.jdbc.Driver";
+            //String url = "jdbc:mysql://localhost:3306/mailDB";
+            String url = "jdbc:mysql://localhost/mailDB?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+            String userName = "root";
+            String password = "password";
+            Class.forName(driver);
+            Connection conn = DriverManager.getConnection(url, userName, password);
+
+            ;
+            return conn;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return null;
+
+
+
+    }
+    public void post() throws Exception {
+
+        try{
+            Connection conn = getConnection();
+            PreparedStatement posted = conn.prepareStatement("INSERT INTO mail (firstName, lastName, email, password, mailboxCapacity)" +
+                    "VALUES ('"+this.firsName+"','"+this.lastName+"','"+this.email+"', '"+this.password+"', '"+this.mailboxCapacity+"')");
+            posted.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            System.out.println("insert completed");
+        }
+    }
 }
